99e6592ef74448bb2e349c759374dc8af79b1be2
João Gabriel <136657802+Jgabriel1800@users.noreply.github.com>
2025-09-25 18:40:45 +0000

    atividade 4 finalizada

---
+++ Atividade4-poo/MultiFor.java
@@ -0,0 +1,25 @@
+public class MultiFor {
+
+    public static void main(String[] args) {
+
+        for(int i = 0; i < 4; i++) {
+
+            for(int j = 4; j > 2; j--) {
+
+                if(i==1) {
+                    System.out.println(i + " " + j);
+
+                    if (j == 3) { 
+                        i++;
+                    }
+                }
+
+                else {
+                    System.out.println(i + " " + j);
+                }
+            }
+        }
+    }
+}
+
+// Resposta é a terceira alternativa 
---
+++ Atividade4-poo/Questao3.java
@@ -0,0 +1,26 @@
+public class Questao3 {
+	public static void main(String[] args) {
+		int x = 0;
+		int y = 30;
+		for(int outer = 0; outer < 3; outer++) {
+			for(int inner = 4; inner > 1; inner--) {
+				x=x+0; // Escolha do candidato 
+				y = y - 2;
+				if(x == 6) {
+					break;
+				}
+				x = x + 3;
+			}
+			y = y - 2;
+		}
+		System.out.println(x + " " + y);
+	}
+}
+
+/*1: x = x+3 -> 54 6
+ * 2: x = x + 6 -> 60 10
+ * 3: x = x+2 -> 45 6 
+ * 4: x++ -> 36 6 
+ * 5: x-- -> 18 6
+ * 6: x = x + 0-> 6 14 
+ */
